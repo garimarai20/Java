b1ace010a240fb8cf8baf923242ce4eee66d8baa
garimarai20 <raigarima.id@gmail.com>
2024-05-02 12:03:21 +0530

    done

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,9 +1,22 @@
 public class BitManipulation {
+    public static void oddOrEven(int n){
+        int bitmask=1;
+        if((n&1) == 0){
+            System.out.println("even number");
+        } else{
+             System.out.println("odd number");
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println(5&6);
-        System.out.println(5^6);
-        System.out.println(5|6);
-        System.out.println();
+        oddOrEven(3);
+        oddOrEven(11);
+        oddOrEven(14);
+        // System.out.println(5&6);
+        // System.out.println(5^6);
+        // System.out.println(5|6);
+        // System.out.println(~5);
+        // System.out.println(5<<2);
     }
     
 }
