55fe9962ddd99b539c0de965ddd15348a95babc3
garimarai20 <raigarima.id@gmail.com>
2024-05-02 11:32:04 +0530

    done

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,6 +1,9 @@
 public class BitManipulation {
     public static void main(String[] args) {
-        
+        System.out.println(5&6);
+        System.out.println(5^6);
+        System.out.println(5|6);
+        System.out.println();
     }
     
 }
