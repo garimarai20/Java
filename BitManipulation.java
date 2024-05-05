3d8d9e5ab607ac8fdd4ff29cc660ceeaadba57e4
garimarai20 <raigarima.id@gmail.com>
2024-05-05 10:37:07 +0530

    done

--- BitManipulation.java
+++ BitManipulation.java
@@ -65,10 +65,21 @@ public static int countSetBits(int n){
 
     } return count;
 }
+public static int fastExpo(int a, int n){
+    int ans =1;
+    while(n>0){
+        if((n&1)!=0){
+            ans= ans*a;
+        }
+        a= a*a;
+        n= n>>1;
+    } return ans;
+
+}
 
 
     public static void main(String[] args) {
-       System.out.println( countSetBits(10));
+       System.out.println( fastExpo(5,3 ));
 
 
 
