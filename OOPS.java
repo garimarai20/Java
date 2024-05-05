fa1631fd5636dab3d30a3f042e9d70bf928a63e2
garimarai20 <raigarima.id@gmail.com>
2024-05-05 11:30:36 +0530

    done

--- OOPS.java
+++ OOPS.java
@@ -1,19 +1,52 @@
+
+
 public class OOPS {
+    public static void main(String[] args) {
+        Pen p1= new Pen();
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+
+    BankAccount myAcc = new BankAccount();
+    myAcc.username= "GarimaRai";
+    myAcc.setPassword("abcdefghi");
+
+    
+    }
     
 }
 
+
+
+class BankAccount{
+   public String username;
+   private String password;
+   
+   public void setPassword (String pwd){
+    password=pwd;
+   }
+}
 class Pen{
     // Properties
-    String color;
-    int tip;
+   private  String color;
+    private int tip;
 
     // Functions
+    String getColor(){
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
     void setColor(String newColor){
         color = newColor;
     }
     void setTip(int newTip){
         tip= newTip;
-    }
+    } 
+    
+
 
 }
 
