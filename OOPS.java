f830f16dc59da67bdbc99fe47ccf49465aab9ce5
garimarai20 <raigarima.id@gmail.com>
2024-05-05 14:33:43 +0530

    done

--- OOPS.java
+++ OOPS.java
@@ -3,7 +3,22 @@
 public class OOPS {
     public static void main(String[] args) {
         Student s1 = new Student();
+        s1.name="Garima";
+        s1.roll=456;
+        s1.password ="abcd";
+        s1.marks[0]=100;
+        s1.marks[1]=90;
+        s1.marks[2]=80;
+
+
+
         
+        Student s2 = new Student(s1);
+        s2.password= "xyz";
+        s1.marks[2]=100;
+        for(int i=0;i<3;i++){
+            System.out.println(s2.marks[i]);
+        }
 
         // Student s1 = new Student("Garima");
         // System.out.println(s1.name);
@@ -27,18 +42,58 @@ public class OOPS {
 
 class Student{
     String name ;
-    int age;
-    float percentage; //cgpa
+    int roll;
+    int[] marks;
+    String password;
 
-    Student(){//String name){
-       System.out.println("Constructor is called");
-        //this.name = name;
-        //System.out.
+
+// copy constructor 
+//DEEP COPY CONSTRUCTOR
+Student(Student s1){
+    marks =new int[3];
+    this.name = s1.name;
+    this.roll = s1.roll;
+    for(int i= 0; i<marks.length;i++){
+        this.marks[i]= s1.marks[i];
     }
 
-    void calPercentage(int phy , int maths , int chem){
-        percentage = (phy+chem+maths)/3;
+}
+
+
+// SHALLOW COPY CONSTRUCTOR
+    // Student(Student s1){
+    //     marks =new int[3];
+    //     this.name = s1.name;
+    //     this.roll = s1.roll;
+    //     this.marks =s1.marks;
+
+    // }
+
+    public Student() {
+        marks =new int[3];
+        System.out.println("Constructor is called");
     }
+
+    public Student(String name, int roll, int[] marks, String password) {
+        marks =new int[3];
+        this.name = name;
+        this.roll = roll;
+        this.marks = marks;
+        this.password = password;
+    }
+    
+
+   // float percentage; //cgpa
+
+    // Student(){//String name){
+    //    System.out.println("Constructor is called");
+    //     //this.name = name;
+    //     //System.out.
+    // }
+
+    // void calPercentage(int phy , int maths , int chem){
+    //     percentage = (phy+chem+maths)/3;
+    // }
 }
 
 
