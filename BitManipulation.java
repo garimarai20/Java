56bb4283a5ff7c83227bb2bc830f163e64b15516
garimarai20 <raigarima.id@gmail.com>
2024-05-05 10:23:31 +0530

    committed

--- BitManipulation.java
+++ BitManipulation.java
@@ -43,8 +43,32 @@ public static int clearIBits(int n,int i){
     return n&bitMask;
 }
 
+public static int clearRangebits(int n , int i , int j ){
+    int a = ((~0)<<(j+1));
+    int b = (1<<i)-1;
+    int bitMask = a|b;
+    return n&bitMask;
+
+} 
+public static boolean isPowerOfTwo(int n ){
+    return (n&(n-1))== 0;
+}
+
+public static int countSetBits(int n){
+
+    int count= 0;
+    while(n>0){
+        if((n&1)!=0){ //check LSB
+            count ++;
+        }
+        n= n>>1;
+
+    } return count;
+}
+
+
     public static void main(String[] args) {
-       System.out.println( clearIBits(15,2));
+       System.out.println( countSetBits(10));
 
 
 
