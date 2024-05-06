b5c09cb1ee834705d678b143bbf1bf5fa632b155
garimarai20 <raigarima.id@gmail.com>
2024-05-06 22:51:52 +0530

    done

--- OOPS3.java
+++ OOPS3.java
@@ -1,6 +1,35 @@
 public class OOPS3 {
     public static void main(String[] args) {
+        Horse h = new Horse();
+        h.eat();
+        h.walk();
+
+        Chicken c = new Chicken();
+        c.eat();
+        c.walk();
         
     }
     
+} 
+
+abstract class Animal{
+    void eat(){
+        System.out.println("hi");
+    }
+
+    abstract void walk();
+    
+}
+
+class Horse extends Animal{
+    void walk(){
+        System.out.println("walks on four legs");
+    }
+
+}
+
+class Chicken extends Animal{
+    void walk(){
+        System.out.println("walks on two legs");
+    }
 }
