c78e16781199ce0d3fcd3b0874d796e315de9aa0
garimarai20 <raigarima.id@gmail.com>
2024-05-06 22:06:37 +0530

    done

---
+++ myPackage/Packages.java
@@ -0,0 +1,19 @@
+package myPackage;
+
+import java.util.*;
+
+public class Packages {
+    public static void main(String[] args) {
+        // Package is a group of similar types of 
+        // classes,interfaces and sub-packages
+
+        /*Scanner is a class with object sc and 
+          Scanner() is a constructor with 
+          System.in as a parameter */
+        Scanner sc = new Scanner(System.in);
+        
+        /* We are using sc object with next() function */
+        String s = sc.next();
+    }
+    
+}
