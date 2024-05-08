08d47d49ae794d52d270f18dadd587fafbb12742
garimarai20 <raigarima.id@gmail.com>
2024-05-08 11:41:00 +0530

    done

--- OOPS4.java
+++ OOPS4.java
@@ -2,19 +2,47 @@ public class OOPS4 {
      
     
     public static void main(String[] args) {
-        Student s1 = new Student();
-       Student.name ="garima";
-        Student s2 = new Student();
-       System.out.println(s2.name);
+        Horse h = new Horse();
+        System.out.println(h.color);
+    
+    
+    
+        //     St s1 = new St();
+    //    s1.name ="garima";
+    //     St s2 = new St();
+    //    System.out.println(s2.name);
+    //    System.out.println(s1.returnPercentage(20, 50, 50));
         
      }
 }
+class Anima {
+    String color;
+    Anima(){
+        System.out.println("animal constructor is called");
+    }
+}
 
-class Student {
-    static String name;
-    int roll;
+class Horse extends Anima{
+
+
+    Horse(){
+        super.color = "brown";
+
+        System.out.println("horse constructor is called");
+    }
+}
+
+
+
+// class St{
+
+//     static int returnPercentage(int math,int phy, int chem){
+//         return (math+phy+chem)/3;
+//     }
+//     static String name;
+//     int roll;
     
     
 
   
-}
+// }
