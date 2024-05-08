8335fc806dd8386d5e70dcfaee457cbd65d3750c
garimarai20 <raigarima.id@gmail.com>
2024-05-08 10:35:49 +0530

    done

---
+++ OOPS4.java
@@ -0,0 +1,20 @@
+public class OOPS4 {
+     
+    
+    public static void main(String[] args) {
+        Student s1 = new Student();
+       Student.name ="garima";
+        Student s2 = new Student();
+       System.out.println(s2.name);
+        
+     }
+}
+
+class Student {
+    static String name;
+    int roll;
+    
+    
+
+  
+}
