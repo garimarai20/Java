678fa39906cb73b1bfa492cdcd7adcdddb38ea3c
garimarai20 <raigarima.id@gmail.com>
2024-05-10 23:17:07 +0530

    done

--- Recursion.java
+++ Recursion.java
@@ -7,6 +7,14 @@ public class Recursion {
         System.out.print(n +" ");
         printDec(n-1);
     }
+    public static void printAsc(int n ){
+        if(n ==1){
+            System.out.print(n);
+            return;
+        } 
+        printAsc(n-1);
+        System.out.print(n+ "");
+    }
 
     public static void main(String[] args) {
         int n = 10;
