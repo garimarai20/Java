94045a592df79f29c5129ad32b1ecbc393f6f3c7
garimarai20 <raigarima.id@gmail.com>
2024-05-22 11:38:24 +0530

    done

--- DivideandConquer.java
+++ DivideandConquer.java
@@ -1,12 +1,23 @@
+public class DivideandConquer {
 public static void PrintArr(int arr[]){
     for(int i =0; i<arr.length;i++){
         System.out.print(arr[i]+" ");
     }
+    System.out.println();
 }
 
-public class DivideandConquer {
+public static void MergeSort(int arr[], int si,int ei){
+    //base case
+
+}
+
+
+
     public static void main(String[] args) {
         int arr[]= {6,3,9,5,2,8};
+        MergeSort(arr, 0, arr.length -1);
+
+        PrintArr(arr);
 
     }
 
