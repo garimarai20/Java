9553d6bc061dd32a5ead9c7197402fe4d3a35c8e
garimarai20 <raigarima.id@gmail.com>
2024-05-22 12:05:56 +0530

    done

--- DivideandConquer.java
+++ DivideandConquer.java
@@ -8,13 +8,13 @@ public static void PrintArr(int arr[]){
 
 public static void MergeSort(int arr[], int si,int ei){
     //base case
-    if(si==ei || si>ei){
+    if(si>=ei){
         return;
     }
     //Kaam
     int mid = si + (ei - si )/2;
     MergeSort(arr, si, mid);
-    MergeSort(arr, mid, ei);
+    MergeSort(arr, mid+1, ei);
     merge(arr, si, mid, ei);
 
 }
@@ -24,6 +24,29 @@ public static void merge(int arr[],int si,int mid,int ei){
     int j = mid +1 ; // iterator for right part
     int k = 0; // iterator for temp arr
 
+    while(i<= mid && j<=ei){
+        if(arr[i]<arr[j]){
+            temp[k]= arr[i];
+            i++; 
+        }else{
+            temp[k]= arr[j];
+            j++;
+        }  k++;
+    }
+//left part
+    while (i<= mid) {
+        temp[k++] =arr[i++]; 
+        
+    }
+// right part
+    while(j<=ei){
+        temp[k++]=arr[j++];
+    }
+//copy temp to original array
+     for(k=0, i=si;k<temp.length; k++,i++){
+        arr[i]= temp[k];
+
+     }
 }
 
 
