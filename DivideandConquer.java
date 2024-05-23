c7dbe5027acfc4af80500f05b34623eb0c92b84c
garimarai20 <raigarima.id@gmail.com>
2024-05-23 10:37:53 +0530

    done

--- DivideandConquer.java
+++ DivideandConquer.java
@@ -63,16 +63,27 @@ public static int partition(int arr[], int si,int ei){
     int pivot = arr[ei];
     int i = si-1; // to make place for elements smallerthan pivot
 
-    for(int j= 0; j<ei;j++){
-        if(arr[j]<= pivot )
+    for(int j= si; j<ei;j++){
+        if(arr[j]<= pivot ){
+            i++;
+            //swap
+            int temp= arr[j];
+            arr[j]= arr[i];
+            arr[i] = temp;
+        }
     }
+    i++;
+    int temp= pivot;
+        arr[ei]= arr[i];
+        arr[i] = temp;
+        return i;
 }
 
 
 
     public static void main(String[] args) {
-        int arr[]= {6,3,9,5,2,8,-2};
-        MergeSort(arr, 0, arr.length -1);
+        int arr[]= {6,3,9,8,2,5,};
+        quicksort(arr, 0, arr.length -1);
 
         PrintArr(arr);
 
