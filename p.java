ed4f39882b665191ca022db79ad5940e91e196c9
garimarai20 <raigarima.id@gmail.com>
2024-06-01 13:21:14 +0530

    done

--- p.java
+++ p.java
@@ -16,23 +16,27 @@ public class p{
     }
 
     public static boolean isSquare(int n){
-        if((Math.sqrt(n*n)) == n){
-            return true;
-        }else{return false;}
+        int s = (int)(Math.sqrt(n));
+        return ((s*s) ==n);
 
     }
 
     public static void main(String[] args) {
-        System.out.println(isSquare(6));
-
-        // int a[]= new int[6];
-        // int po2 = 1 ;
-        // int po3 = 1 ;
-        // for(int i=0; i<a.length;i++){
-        //     if((isPrime(i+1)) == true){
-        //         a[i]= po2;
-        //         po2 = po2*2;
-        //     }
+        
+        int a[]= new int[6];
+        int po2 = 1 ;
+        int po3 = 1 ;
+        for(int i=0; i<a.length;i++){
+            if((isPrime(i+1)) == true){
+                a[i]= po2;
+                po2 = po2*2;
+            }
+           else if((isSquare(i+1))== true){
+                a[i] = po3;
+                po3 = po3 *3;
+            } else{
+                
+            } 
 
         // }
 
