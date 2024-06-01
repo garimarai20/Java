472b378d7ccd19a3e3701eecce79b7d9ecbedf47
garimarai20 <raigarima.id@gmail.com>
2024-06-01 13:09:25 +0530

    done

---
+++ p.java
@@ -0,0 +1,28 @@
+public class p{
+
+    public static boolean isPrime(int n){
+
+        for(int i =2; i<n;i++){
+            if(n%i==0){
+                return false;  
+            }
+            else{
+                return true;
+            }
+        } return true;
+    }
+
+    public static void main(String[] args) {
+        int a[]= new int[6];
+        int po2 = 1 ;
+        int po3 = 1 ;
+        for(int i=0; i<a.length;i++){
+            if((isPrime(i+1)) == true){
+                
+            }
+        }
+
+
+
+    }
+}
