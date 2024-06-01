f3fb953be1141a45786e856aa3bdd6d0679df686
garimarai20 <raigarima.id@gmail.com>
2024-06-01 11:25:27 +0530

    done

--- DivideandConquer.java
+++ DivideandConquer.java
@@ -116,9 +116,11 @@ public static int search(int arr[], int tar , int si, int ei){
 
 
     public static void main(String[] args) {
-        int arr[]={
-            -2,-1,2,4,5,6,7,8
-        };
+
+        
+        // int arr[]={
+        //     -2,-1,2,4,5,6,7,8
+        // };
 
         // int arr[] = {4,5,6,7,0,1,2};
         // int tar= 0;
