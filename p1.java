da01bf7bf090fb10d84f533d41560d475588fa52
garimarai20 <raigarima.id@gmail.com>
2024-06-02 20:33:08 +0530

    done

--- p1.java
+++ p1.java
@@ -1,1 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
+
+public class p1{
+    public static void main(String[] args) {
+        System.out.println("Enter size of array");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n[]= new int[a];
+        for(int i=0; i<a;)
+        
+    }
+}
