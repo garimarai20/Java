759db07bbd0d793bd3e809d7ead408066b1eb041
garimarai20 <raigarima.id@gmail.com>
2024-06-02 20:37:58 +0530

    done

--- p1.java
+++ p1.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class p1{
     public static void main(String[] args) {
-        System.out.println("Enter size of array");
+        System.out.println("Enter size of array: ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int n[]= new int[a];
-        for(int i=0; i<a;)
+        System.out.println("Enter elements of array: ");
+        for(int i=0; i<a;i++){
+            n[i]=sc.nextInt();
+        }
+        System.out.print("Array: " );
+        for(int i=0 ; i<a;i++){
+            System.out.print(n[i]+" " );
+        }
         
     }
 }
