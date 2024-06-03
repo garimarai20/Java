cdbf2f3909c10e04c1fa1e194397ea3f3e13a5b5
garimarai20 <raigarima.id@gmail.com>
2024-06-03 06:14:52 +0530

    done

--- BackTracking.java
+++ BackTracking.java
@@ -1,3 +1,11 @@
 public class BackTracking {
-    
+
+    public static void changeArr(int[] a, int i,int val){
+        
+    }
+
+
+    public static void main(String[] args) {
+        int a[]= new int[5];
+    }
 }
