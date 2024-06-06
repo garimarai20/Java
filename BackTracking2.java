658d0f19b17e4d585b638cd7846def02f93a3dad
garimarai20 <raigarima.id@gmail.com>
2024-06-06 15:34:48 +0530

    done

--- BackTracking2.java
+++ BackTracking2.java
@@ -4,6 +4,9 @@ public class BackTracking2{
     public static void findSubsets(String str, String ans, int i){
         //base case
         if(i==str.length()){
+            if(ans.length() ==0){
+                System.out.println("null");
+            }
             System.out.println(ans);
             return;
         }
@@ -13,7 +16,7 @@ public class BackTracking2{
         findSubsets(str, ans+str.charAt(i), i+1);
 
         //No choice
-        findSubsets(str,ans,i+1);
+        findSubsets(str,ans,i+1); 
 
     }
 
