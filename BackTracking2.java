7310641e1b741d1c4e62a54420bd28b2863954c4
garimarai20 <raigarima.id@gmail.com>
2024-06-12 20:57:56 +0530

    done

--- BackTracking2.java
+++ BackTracking2.java
@@ -4,17 +4,22 @@ public class BackTracking2{
 
     public static void findPermutation(String str, String ans){
       //base case  
-       
+      if(str.length()==0){
+      System.out.println(ans);
+      return;
+      }
       //recursion 
         for(int i =0; i<str.length(); i++){
             char curr = str.charAt(i);
-            str= str.substring(0,i) +str.substring(i+1) ;
+            String nstr= str.substring(0,i) +str.substring(i+1) ;
+            findPermutation(nstr, ans+curr);
 
         }
     }
 
     public static void main(String[] args) {
         String str ="abc";
+        findPermutation(str, "");
     }
 }
 //     public static void findSubsets(String str, String ans, int i){
