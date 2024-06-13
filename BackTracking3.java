5498b92afbbc0e9face51d819e37d9f09afd0580
garimarai20 <raigarima.id@gmail.com>
2024-06-13 22:01:12 +0530

    done

--- BackTracking3.java
+++ BackTracking3.java
@@ -2,10 +2,25 @@ public class BackTracking3 {
 
     public static boolean isSafe(char board[][], int row, int col){
         // vertical up
-
+        for(int i =row -1;i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
         //left up
+        for(int i = row-1,j =col-1; i>=0 && j>=0; i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
 
         //right up
+        for(int i = row-1, j =col+1; i>=0 && j<board.length;i--, j++ ){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void nQueens(char board[][], int row){
