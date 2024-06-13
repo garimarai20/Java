a31b8baa94dfd2def010851172fa2e2684c772af
garimarai20 <raigarima.id@gmail.com>
2024-06-13 21:24:27 +0530

    done

--- BackTracking3.java
+++ BackTracking3.java
@@ -1,4 +1,11 @@
 public class BackTracking3 {
+
+    public static void nQueens(char board[][], int row){
+
+        for(int j=0;j<board.length; j++){
+            board[i][j] = 
+        }
+    }
     public static void main(String[] args) {
         int n = 3;
         char board[][] = new char [n][n];
