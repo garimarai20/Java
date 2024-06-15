a165045b6c1a4a0284690cecb7a71dcc93851b29
garimarai20 <raigarima.id@gmail.com>
2024-06-15 22:19:36 +0530

    done

--- BackTracking3.java
+++ BackTracking3.java
@@ -50,7 +50,7 @@ public class BackTracking3 {
         }
     }
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5 ;
         char board[][] = new char [n][n];
         // initialize
         for(int i=0; i<n; i++){
