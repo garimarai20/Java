07e5f6279f4c72c0015b899f9ff53c30c3111bf2
garimarai20 <raigarima.id@gmail.com>
2024-07-24 20:59:27 +0530

    committed

---
+++ ArrayLists.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class ArrayLists {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> lst1 = new ArrayList<>();
+        
+    }
+}
