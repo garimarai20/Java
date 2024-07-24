da942e6e568e954c15f8a121a6065ba938081c56
garimarai20 <raigarima.id@gmail.com>
2024-07-24 21:07:07 +0530

    committed

--- ArrayLists.java
+++ ArrayLists.java
@@ -4,5 +4,16 @@ public class ArrayLists {
         ArrayList<Integer> list = new ArrayList<>();
         ArrayList<String> lst1 = new ArrayList<>();
         
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+
+        System.out.println(list.get(2));
+        System.out.println(list.remove(2));
+        System.out.println(list);
+        
     }
 }
